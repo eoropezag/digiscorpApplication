3cb9e892ce30198ffacb070f400855585aac8c6c
Emmanuel Oropeza <eoropezag3@gmail.com>
2023-02-22 22:13:43 -0600

    Firts commit

---
+++ src/main/java/com/example/demo/DigiscorpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiscorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiscorpApplication.class, args);
+	}
+
+}
